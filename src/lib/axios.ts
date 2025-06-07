0ec8e626adcce1bc5ef8ca64e57b61fb1f302059
dhawal <dhawalbathre333@gmail.com>
2025-06-07 11:05:32 +0530

    added railway deployment config file

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL : 'https://rishta-backend.onrender.com',
+    // baseURL : 'https://rishta-backend.onrender.com',
+    baseURL : 'http://localhost:3000',
     headers : {
         'Content-Type': 'application/json'
     }
